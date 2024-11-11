0c7325382096e00e5f21589e3a86749c9b4849bd
Guino-Elvis <ginoyujra38@gmail.com>
2024-11-10 20:22:02 -0500

    imagenhjghjghjh

--- ventas/src/main/java/com/example/ventas/VentasApplication.java
+++ ventas/src/main/java/com/example/ventas/VentasApplication.java
@@ -2,6 +2,11 @@ package com.example.ventas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class VentasApplication {
@@ -10,4 +15,13 @@ public class VentasApplication {
 		SpringApplication.run(VentasApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI custoOpenAPI() {
+		return new OpenAPI().info(new Info()
+				.title("OPEN API MICROSERVICIOS BLIBIOTECA")
+				.version("0.0.1")
+				.description("servicio web biblioteca")
+				.termsOfService("http://swagger.io/terms")
+				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+	}
 }
